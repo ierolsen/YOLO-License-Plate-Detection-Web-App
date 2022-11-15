90dee8e5fd8cc8cdfba17d0f0051ff3451d42335
Erol <ierolsen@outlook.com>
2022-11-15 18:58:53 +0300

    add yolo

--- app.py
+++ app.py
@@ -1,27 +1,30 @@
 from flask import Flask, render_template, request
 import os
 from deeplearning import OCR
+from yolo_prediction import object_detection
 
 # webserver gateway interface
 app = Flask(__name__)
 
 BASE_PATH = os.getcwd()
-UPLOAD_PATH = os.path.join(BASE_PATH, "static/upload")
+UPLOAD_PATH = os.path.join(BASE_PATH,'static/upload/')
 
-@app.route("/", methods=["POST", "GET"])
+
+@app.route('/',methods=['POST','GET'])
 def index():
-    
-    if request.method == "POST":
-        upload_file = request.files["image_name"]
+    if request.method == 'POST':
+        upload_file = request.files['image_name']
         filename = upload_file.filename
-        path_save = os.path.join(UPLOAD_PATH, filename)
+        path_save = os.path.join(UPLOAD_PATH,filename)
         upload_file.save(path_save)
+        text_list = object_detection(path_save,filename)
         
-        text = OCR(path_save, filename)
-        
-        return render_template("index.html", upload=True, upload_image=filename, text=text)
-        
-    return render_template("index.html", upload=False)
+        print(text_list)
 
-if __name__ == "__main__":
+        return render_template('index.html',upload=True,upload_image=filename,text=text_list,no=len(text_list))
+
+    return render_template('index.html',upload=False)
+
+
+if __name__ =="__main__":
     app.run(debug=True)
